7b53b818d4cd4871e4bebd0a5aed6169d1a94510
ajbanks <$Ferrari4>
2019-02-05 22:25:40 +0000

    added t=real time processing class

--- src/footballstats/RealTimeProcessing.java
+++ src/footballstats/RealTimeProcessing.java
@@ -1,23 +1,49 @@
 package footballstats;
 
+import java.util.ArrayList;
+
 public class RealTimeProcessing {
     PosLogToArff pos;
     String logfile;
     double lastEndTime;
+    ArrayList<Integer> classifications;
+    Boolean classifyActions;
+    double timeSinceLastPosition;
+    double positionWindow;
 
     public RealTimeProcessing (PosLogToArff pos, String logfile, double positionWindow){
         this.logfile = logfile;
         this.pos = pos;
         this.lastEndTime = 0.0;
+        this.classifications = new ArrayList<Integer>();
+        this.classifyActions = true;
+        this.timeSinceLastPosition = 0.0;
+        this.positionWindow = positionWindow;
 
     }
 
 
-    public void beginProcessing(){}
+    public void beginProcessing() {
+        while (classifyActions = true) {
+            if (timeSinceLastPosition >= positionWindow) {
+                int classification = getClassificationOfPosition(getNextSetOfPositions(0.0, "file"));
+                classifications.add(classification);
+                System.out.println(classification);
+                lastEndTime = lastEndTime + positionWindow;
+                timeSinceLastPosition = 0.0;
+            }
 
-    public void createPositionImage(double [] positions){}
+        }
+    }
 
-    public void getNextSetOfPositions(double positionWindow){
+    public double[][] createPositionImage(double [] positions){
+        double [][] image = {{0.0},{0.0}};
+        return image;
+    }
+
+    public double [] getNextSetOfPositions(double positionWindow, String logfile){
+        double [] positions = {0.0, 0.1, 0.2, 0.3} ;
+        return positions;
     }
 
     public void changeLastEndTime(double time){
@@ -28,7 +54,14 @@ public class RealTimeProcessing {
         this.lastEndTime = 0.0;
     }
 
+    public int getClassificationOfPosition(double [] positions){
+        double [][] image = createPositionImage(positions);
+        int classification = 0;
+        return classification;
+    }
+
     public void readLogFromLastTime(){
 
     }
+
 }
